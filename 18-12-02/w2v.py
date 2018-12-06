213294f4ee6e23cc98be3713c4d6f8288c7bc9b6
wk <k_wang93@163.com>
2018-12-06 21:13:59 +0800

    Add graph using t-SNE dimensionality reduction

--- 18-12-02/w2v.py
+++ 18-12-02/w2v.py
@@ -5,6 +5,8 @@ import gensim
 from gensim.models import Word2Vec
 from gensim.models import KeyedVectors
 from gensim.models.word2vec import LineSentence
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
 
 
 # Cutting words and saving to corpus file
@@ -48,7 +50,48 @@ def get_model_from_file():
     return model
 
 
+def tsne_plot(model):
+    "Creates and TSNE model and plots it"
+    labels = []
+    tokens = []
+
+    ii = 0
+    for word in model.wv.vocab:
+        if ii > 200: break
+        tokens.append(model[word])
+        labels.append(word)
+        ii += 1
+
+    tsne_model = TSNE(perplexity=40, n_components=2, init='pca', n_iter=2500, random_state=23)
+    new_values = tsne_model.fit_transform(tokens)
+
+    x = []
+    y = []
+    for value in new_values:
+        x.append(value[0])
+        y.append(value[1])
+
+    plt.figure(figsize=(16, 16))
+    for i in range(len(x)):
+        plt.scatter(x[i], y[i])
+        plt.annotate(labels[i],
+                     xy=(x[i], y[i]),
+                     xytext=(5, 2),
+                     textcoords='offset points',
+                     ha='right',
+                     va='bottom')
+    plt.show()
+
+
 if __name__ == '__main__':
     w2v_model = get_model_from_file()
     print(w2v_model.wv['数学'])
     print(w2v_model.wv.most_similar('数学'))
+
+    font = {'family': 'simhei',
+            'weight': 'regular',
+            'size': '12'}
+    plt.rc('font', **font)
+    plt.rc('axes', unicode_minus=False)
+
+    tsne_plot(w2v_model)
