12a0d777df7bf2e906155a9ed0d51fbcafd7c5d6
wk <k_wang93@163.com>
2019-01-22 22:13:27 +0800

    KNN model optimization

--- 19-01-13/simi_detect.py
+++ 19-01-13/simi_detect.py
@@ -1,6 +1,7 @@
 import random
 import re
 from collections import Counter
+from collections import defaultdict
 
 import jieba
 import numpy as np
@@ -16,24 +17,43 @@ def get_w2v_model(filename):
 
 
 def rm_spec(sent):
-    ret = re.sub('[\n\s+\.\!\/_,$%^*(+\"\')]+|[+——\-()?【】《》“”！，。？、~@#￥%……&*（）]+', '', sent)
+    ret = re.sub('[\\n\s+\.\!\/_,$%^*(+\"\')]+|[+——\-()?【】《》“”！，。？、~@#￥%……&*（）]+', '', sent)
     if ret:
         return ret
     return ''
 
 
-def write_news_corpus_to_file(filename, news, lst):
+def split_sent(sent):
+    sents = re.split('[。？！\n]', sent)
     ret = []
-    with open(filename, 'w', encoding='utf-8') as fout:
-        for i in lst:
-            sent = news['content'][i]
-            sent = rm_spec(sent)
-            wd_lst = jieba.lcut(sent)
-            if wd_lst:
-                fout.write(' '.join(wd_lst))
-                fout.write('\n')
-                ret.append(i)
+    for s in sents:
+        sl = jieba.lcut(s)
+        slt = []
+        for item in sl:
+            sr = rm_spec(item)
+            if sr:
+                slt.append(sr)
+        if slt:
+            ret.append(' '.join(slt))
     return ret
+
+
+def process_news_corpus(filename, news, lst):
+    di = defaultdict(list)
+    xna_lst = []
+    count = 0
+    with open(filename, 'w', encoding='utf-8') as fout:
+        for i, item in enumerate(lst):
+            sent = news['content'][item]
+            sents = split_sent(sent)
+            if sents:
+                di[i].append(count)
+                count += len(sents)
+                di[i].append(count)
+                if '新华社' in news['source'][item] or '新华网' in news['source'][item]:
+                    xna_lst.append(i)
+                fout.write('\n'.join(sents) + '\n')
+    return di, xna_lst
 
 
 def add_more_train_corpus(model, filename):
@@ -42,6 +62,16 @@ def add_more_train_corpus(model, filename):
         for line in fin.readlines():
             sen_list.append(line.split())
     model.train(sentences=sen_list, total_examples=len(sen_list), epochs=1)
+
+
+def get_index_lst_from_dict(di, lst):
+    ret = []
+    for i in lst:
+        start = di[i][0]
+        end = di[i][1]
+        for t in range(start, end):
+            ret.append(t)
+    return ret
 
 
 def word_freq(corpus_file):
@@ -91,14 +121,6 @@ def get_list_with_content(news):
     return ret
 
 
-def get_xna_news_index(news, lst):
-    ret = []
-    for i in range(len(lst)):
-        if '新华社' in news['source'][lst[i]] or '新华网' in news['source'][lst[i]]:
-            ret.append(i)
-    return ret
-
-
 def get_tfidf_mat(news, lst):
     corpus = []
     for i in lst:
@@ -108,7 +130,7 @@ def get_tfidf_mat(news, lst):
     return ret
 
 
-def get_remain_samples_list(all_samples, part_samples):
+def get_remain_list(all_samples, part_samples):
     ret = []
     for i in all_samples:
         if i not in part_samples:
@@ -116,38 +138,72 @@ def get_remain_samples_list(all_samples, part_samples):
     return ret
 
 
+def lst2file(filename, lst):
+    with open(filename, 'w') as fout:
+        fout.write('\n'.join([str(i) for i in lst]) + '\n')
+
+
 # Classify using kNN
 def KNNClassifier(mat, xna_trn_lst, otr_trn_lst, xna_test_lst, otr_test_lst):
     X_train = mat[xna_trn_lst + otr_trn_lst, :]
-    y_train = np.array([0] * X_train.shape[0])
+    Y = np.array([0] * mat.shape[0])
     for i in xna_trn_lst:
-        y_train[i] = 1
+        Y[i] = 1
+    y_train = Y[xna_trn_lst + otr_trn_lst]
 
-    neigh = KNeighborsClassifier(n_neighbors=20)
+    neigh = KNeighborsClassifier(n_neighbors=10)
     neigh.fit(X_train, y_train)
-    for i in range(10):
-        print(mat[xna_test_lst[i]])
-        print(neigh.predict([mat[xna_test_lst[i]]]))
+
+    xna_test = []
+    otr_test = []
+    for i in range(len(xna_test_lst)):
+        xna_test.append(neigh.predict([mat[xna_test_lst[i]]])[0])
+    for i in range(len(otr_test_lst)):
+        otr_test.append(neigh.predict([mat[otr_test_lst[i]]])[0])
+    return xna_test, otr_test
 
 
+# Import original corpus
 news_df = pd.read_csv('sqlResult_1558435.csv', encoding='gb18030')
-lst_with_content = get_list_with_content(news_df)
-lst_with_content = write_news_corpus_to_file('news_corpus.txt', news_df, lst_with_content)
 
-# Find XNA news in news corpus
-xna_news_lst = get_xna_news_index(news_df, lst_with_content)
+# Get news list which has source and content
+lst_with_content = get_list_with_content(news_df)
+
+# Build news index dict of corpus file, and filter XNA news
+index_dict, xna_news_lst = process_news_corpus('news_corpus.txt', news_df, lst_with_content)
+otr_news_lst = get_remain_list(list(index_dict.keys()), xna_news_lst)
+
+# Write temp results to file
+# with open('index_dict.txt', 'w') as fout:
+#     for k in index_dict:
+#         fout.write(str(k) + ':' + ','.join([str(i) for i in index_dict[k]]) + '\n')
+#
+# with open('xna_lst.txt', 'w') as fout:
+#     fout.write('\n'.join([str(i) for i in xna_news_lst]))
+#     fout.write('\n')
 
 # Set 90% samples for training, 10% for testing
-samples_n = int(len(xna_news_lst) * 0.9)
-xna_samples_train = random.sample([i for i in range(len(xna_news_lst))], samples_n)
-xna_samples_test = get_remain_samples_list(xna_news_lst, xna_samples_train)
-other_samples_n = int((len(lst_with_content) - len(xna_news_lst)) * 0.9)
-other_samples = get_remain_samples_list([i for i in range(len(lst_with_content))], xna_news_lst)
-other_samples_train = random.sample(other_samples, other_samples_n)
-other_samples_test = get_remain_samples_list(other_samples, other_samples_train)
+xna_news_sent_lst = get_index_lst_from_dict(index_dict, xna_news_lst)
+otr_news_sent_lst = get_index_lst_from_dict(index_dict, otr_news_lst)
+
+samples_n = int(len(xna_news_sent_lst) * 0.9)
+
+xna_samples_train = random.sample(xna_news_sent_lst, samples_n)
+xna_samples_test = get_remain_list(xna_news_sent_lst, xna_samples_train)
+
+otr_samples = random.sample(otr_news_sent_lst, len(xna_news_sent_lst))
+otr_samples_train = random.sample(otr_samples, samples_n)
+otr_samples_test = get_remain_list(otr_samples, otr_samples_train)
+
+print('XNA news amount for training:', len(xna_samples_train))
+print('Other news amount for training:', len(otr_samples_train))
+
+# f = open('train_lst.txt', 'w')
+# f.write('\n'.join([str(i) for i in xna_train_lst]))
+# f.close()
 
 # Get tfidf vector
-# tfidf method for large corpus has large memory footprint, need to replace it with w2v embedding
+# tfidf method for large corpus has large memory footprint, better to replace it with w2v embedding
 # tfidf_mat = get_tfidf_mat(news_df, lst_with_content).toarray()
 
 w2v_model = get_w2v_model('wiki_w2v.model')
@@ -156,4 +212,7 @@ get_word_prob = word_freq('news_corpus.txt')
 write_sent_vec_to_file('sent_vec.txt', w2v_model, get_word_prob, 'news_corpus.txt')
 all_sent_mat = get_all_sent_vec('sent_vec.txt')
 
-KNNClassifier(all_sent_mat, xna_samples_train, other_samples_train, xna_samples_test, other_samples_test)
+xna_test_result, otr_test_result = KNNClassifier(all_sent_mat, xna_samples_train, otr_samples_train, xna_samples_test,
+                                                 otr_samples_test)
+lst2file('xna_test_result.txt', xna_test_result)
+lst2file('otr_test_result.txt', otr_test_result)
