ef8ca3c461f04de780478adb20e56693c9267160
wk <k_wang93@163.com>
2019-01-26 00:23:05 +0800

    XGBoost model optimization

--- 19-01-13/simi_detect.py
+++ 19-01-13/simi_detect.py
@@ -1,7 +1,7 @@
 # 抄袭判定算法流程
 # 1. 新闻预料处理，标记出新闻来源，来源为“新华社”或“新华网”的均视为新华社消息，标记为1，其他来源新闻标记为0
 # 2. 语料分句及分词之后写入文件，用作后面计算句子向量
-# 3. 训练样本与测试样本筛选，从新华社新闻中随机抽取90%样本作为训练样本，其余为测试样本，其他新闻中选取与新华社训练相同样本数参与模型训练
+# 3. 训练样本与测试样本筛选，从新华社新闻和其他新闻中选取训练样本参与训练，选取测试样本作测试用
 # 4. 由于TF-IDF矩阵维数大、训练速度慢，本程序使用Word2Vec及词频率加权计算句子向量，作模型的输入，W2V使用之前训练的Wiki数据模型，并增加新的新闻语料训练
 # 5. 抄袭判定规则：如果一篇新闻中有超过30%的句子判定为抄袭，则认为这篇新闻抄袭了新华社的新闻
 # 6. 模型质量判断：计算模型判定的精确率和召回率判断模型的质量
@@ -62,6 +62,7 @@ def process_news_corpus(filename, news, lst):
     with open(filename, 'w', encoding='utf-8') as fout:
         for i, item in enumerate(lst):
             sent = news['content'][item]
+            sent = re.sub('\\n', '', sent)
             sents = split_sent(sent)
             if sents:
                 di[i].append(count)
@@ -185,6 +186,7 @@ def get_precision_and_recall(xna_test_res, otr_test_res):
     fn = cc1[0]
     preci = tp / (tp + fp)
     recal = tp / (tp + fn)
+
     return preci, recal
 
 
@@ -213,24 +215,27 @@ def RFClassifier(xTrain, yTrain):
 
 
 class XGBClassifier:
-    def __init__(self, xTrain, yTrain):
+    def __init__(self, xTrain, yTrain, xVali, yVali):
         dtrain = xgb.DMatrix(xTrain, label=yTrain)
-        params = {'objective': 'binary:logitraw', 'silent': 1, 'n_estimators': 1000,
-                  'max_depth': 8}
-        self.clf = xgb.train(params, dtrain)
+        dVali = xgb.DMatrix(xVali, label=yVali)
+        params = {'objective': 'binary:logistic', 'silent': 1, 'nthread': 4,
+                  'max_depth': 3, 'eta': 1.2, 'min_child_weight': 2}
+        evallist = [(dVali, 'eval'), (dtrain, 'train')]
+        self.clf = xgb.train(params, dtrain, num_boost_round=100, evals=evallist)
 
     def predict(self, mat):
         test = xgb.DMatrix(mat)
         res = self.clf.predict(test)
-        return res.astype(int)
+        res = (res >= 0.5) * 1
+        return res
 
 
 # Classify main process
 def main_func(classifier, mat, idx_dict, xna_trn_lst, otr_trn_lst, xna_test_lst, otr_test_lst):
     xna_trn_sent_lst = get_index_lst_from_dict(idx_dict, xna_trn_lst)
     otr_trn_sent_lst = get_index_lst_from_dict(idx_dict, otr_trn_lst)
-    # print('XNA news amount for training:', len(xna_trn_sent_lst))
-    # print('Other news amount for training:', len(otr_trn_sent_lst))
+    # print('XNA news sentences for training:', len(xna_trn_sent_lst))
+    # print('Other news sentences for training:', len(otr_trn_sent_lst))
 
     X_train = mat[xna_trn_sent_lst + otr_trn_sent_lst, :]
     Y = np.array([0] * mat.shape[0])
@@ -274,6 +279,59 @@ def main_func(classifier, mat, idx_dict, xna_trn_lst, otr_trn_lst, xna_test_lst,
     # lst2file('otr_result.txt', otr_test)
 
 
+def xgb_func(classifier, mat, idx_dict, xna_trn_lst, otr_trn_lst, xna_test_lst, otr_test_lst,
+             xna_vali_lst, otr_vali_lst):
+    xna_trn_sent_lst = get_index_lst_from_dict(idx_dict, xna_trn_lst)
+    otr_trn_sent_lst = get_index_lst_from_dict(idx_dict, otr_trn_lst)
+    # print('XNA news sentences for training:', len(xna_trn_sent_lst))
+    # print('Other news sentences for training:', len(otr_trn_sent_lst))
+
+    X_train = mat[xna_trn_sent_lst + otr_trn_sent_lst, :]
+    Y = np.array([0] * mat.shape[0])
+    for i in xna_trn_sent_lst: Y[i] = 1
+    y_train = Y[xna_trn_sent_lst + otr_trn_sent_lst]
+
+    xna_vali_sent_lst = get_index_lst_from_dict(idx_dict, xna_vali_lst)
+    otr_vali_sent_lst = get_index_lst_from_dict(idx_dict, otr_vali_lst)
+    X_vali = mat[xna_vali_sent_lst + otr_vali_sent_lst, :]
+    Y_vali = np.array([0] * mat.shape[0])
+    for i in xna_vali_sent_lst: Y_vali[i] = 1
+    y_vali = Y_vali[xna_vali_sent_lst + otr_vali_sent_lst]
+
+    trainedModel = classifier(X_train, y_train, X_vali, y_vali)
+
+    xna_test = []
+    otr_test = []
+    threshold = 0.3  # Similiar sentence amount over 30% is classified as plagiarized
+
+    for xt in xna_test_lst:
+        sent_lst = get_idx_lst_from_dict(idx_dict, xt)
+        scores = []
+        for si in sent_lst:
+            sco = trainedModel.predict([mat[si]])[0]
+            scores.append(sco)
+        cc = Counter(scores)
+        if cc[1] / len(sent_lst) > threshold:
+            xna_test.append(1)
+        else:
+            xna_test.append(0)
+
+    for ot in otr_test_lst:
+        sent_lst = get_idx_lst_from_dict(idx_dict, ot)
+        scores = []
+        for si in sent_lst:
+            sco = trainedModel.predict([mat[si]])[0]
+            scores.append(sco)
+        cc = Counter(scores)
+        if cc[1] / len(sent_lst) > threshold:
+            otr_test.append(1)
+        else:
+            otr_test.append(0)
+
+    precision, recall = get_precision_and_recall(xna_test, otr_test)
+    print(classifier.__name__ + ' precision {}, recall {}'.format(precision, recall))
+
+
 # Import original corpus
 news_df = pd.read_csv('sqlResult_1558435.csv', encoding='gb18030')
 
@@ -282,7 +340,7 @@ lst_with_content = get_list_with_content(news_df)
 
 # Build news index dict of corpus file, and filter XNA news
 index_dict, xna_news_lst = process_news_corpus('news_corpus.txt', news_df, lst_with_content)
-otr_news_lst = get_remain_list(list(index_dict.keys()), xna_news_lst)
+otr_news_lst = list(set(index_dict.keys()) - set(xna_news_lst))
 
 # Write temp results to file
 # with open('index_dict.txt', 'w') as fout:
@@ -293,15 +351,13 @@ otr_news_lst = get_remain_list(list(index_dict.keys()), xna_news_lst)
 #     fout.write('\n'.join([str(i) for i in xna_news_lst]))
 #     fout.write('\n')
 
-# Set 90% samples for training, 10% for testing
-samples_n = int(len(xna_news_lst) * 0.9)
-
-xna_samples_train = random.sample(xna_news_lst, samples_n)
-xna_samples_test = get_remain_list(xna_news_lst, xna_samples_train)
-
-otr_samples = random.sample(otr_news_lst, len(xna_news_lst))
-otr_samples_train = random.sample(otr_samples, samples_n)
-otr_samples_test = get_remain_list(otr_samples, otr_samples_train)
+# Train and test samples
+sample_train_n = 5000
+sample_test_n = 500
+xna_samples_train = random.sample(xna_news_lst, sample_train_n)
+xna_samples_test = random.sample(set(xna_news_lst) - set(xna_samples_train), sample_test_n)
+otr_samples_train = random.sample(otr_news_lst, sample_train_n)
+otr_samples_test = random.sample(set(otr_news_lst) - set(otr_samples_train), sample_test_n)
 
 # f = open('train_lst.txt', 'w')
 # f.write('\n'.join([str(i) for i in xna_train_lst]))
@@ -320,7 +376,7 @@ all_sent_mat = get_sent_vec(w2v_model, get_word_prob, 'news_corpus.txt')
 main_func(KNNClassifier, all_sent_mat, index_dict, xna_samples_train, otr_samples_train,
           xna_samples_test, otr_samples_test)
 
-## SVM
+## SVM, slow than others
 main_func(SVMClassifier, all_sent_mat, index_dict, xna_samples_train, otr_samples_train,
           xna_samples_test, otr_samples_test)
 
@@ -333,5 +389,10 @@ main_func(RFClassifier, all_sent_mat, index_dict, xna_samples_train, otr_samples
           xna_samples_test, otr_samples_test)
 
 ## XGBoost
-main_func(XGBClassifier, all_sent_mat, index_dict, xna_samples_train, otr_samples_train,
-          xna_samples_test, otr_samples_test)
+xna_samples_vali = random.sample(set(xna_news_lst) - set(xna_samples_train) - set(xna_samples_test),
+                                 sample_test_n)
+otr_samples_vali = random.sample(set(otr_news_lst) - set(otr_samples_train) - set(xna_samples_test),
+                                 sample_test_n)
+
+xgb_func(XGBClassifier, all_sent_mat, index_dict, xna_samples_train, otr_samples_train,
+         xna_samples_test, otr_samples_test, xna_samples_vali, otr_samples_vali)
